e48c248e03284f9eabecf9a9274e219027cfbafb
yn15 <yn15@naver.com>
2023-02-17 12:55:03 +0900

    Modify: call 갯수

--- facility/fclty_controller.py
+++ facility/fclty_controller.py
@@ -2,7 +2,7 @@ from fclty_tools import *
 from fclty_api_caller import FcltyCaller
 from fclty_dao import *
 
-new_list = FcltyCaller().get_id_list(100)
+new_list = FcltyCaller().get_id_list(10000)
 old_list = FcltyDAO().select_id_list()
 added_facilities = ListCheck().get_added_list(new_list, old_list)
 
