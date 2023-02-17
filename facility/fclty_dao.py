582b6cdc9fc7d850555099db9723f39fb469f53d
yn15 <yn15@naver.com>
2023-02-17 15:16:44 +0900

    Fix: database 예외 처리 - database connection close 처리 - try catch 문 삽입

--- facility/fclty_dao.py
+++ facility/fclty_dao.py
@@ -16,21 +16,33 @@ class FcltyDAO:
             db=os.environ['DATABASE_NAME'],
             charset='utf8'
         )
-        self.curs = self.conn.cursor(cursors.DictCursor)
+        try:
+            self.curs = self.conn.cursor(cursors.DictCursor)
+        except Exception as e:
+            print("Exception occured:{}".format(e))
 
 
     def insert_data(self, data):
-        sql = "INSERT INTO facility(facility_id, facility_name, facility_telno, facility_relateurl, facility_address, facility_latitude, facility_longitude) " \
-              "VALUES (%(facility_id)s, %(facility_name)s, %(facility_telno)s, %(facility_relateurl)s, %(facility_address)s, %(facility_latitude)s, %(facility_longitude)s);"
-        self.curs.executemany(sql, data)
-        self.conn.commit()
+        try:
+            sql = "INSERT INTO facility(facility_id, facility_name, facility_telno, facility_relateurl, facility_address, facility_latitude, facility_longitude) " \
+                  "VALUES (%(facility_id)s, %(facility_name)s, %(facility_telno)s, %(facility_relateurl)s, %(facility_address)s, %(facility_latitude)s, %(facility_longitude)s);"
+            self.curs.executemany(sql, data)
+            self.conn.commit()
+        except Exception as e:
+            print("Exception occured:{}".format(e))
+        finally:
+            self.conn.close()
 
     def select_id_list(self):
-        sql = "SELECT facility_id FROM facility"
-        self.curs.execute(sql)
-        list = []
-        result = self.curs.fetchall()
-        for data in result:
-            list.append(data["facility_id"])
-
-        return list
+        try:
+            sql = "SELECT facility_id FROM facility"
+            self.curs.execute(sql)
+            list = []
+            result = self.curs.fetchall()
+            for data in result:
+                list.append(data["facility_id"])
+        except Exception as e:
+            print("Exception occured:{}".format(e))
+        finally:
+            self.conn.close()
+            return list
